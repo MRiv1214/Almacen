081d6c5c76b33fa8c86944974ab2dce2eb990a81
JordyPirata <jordi77alva@gmail.com>
2023-11-15 22:51:09 -0600

    Refactor Student Menu

--- src/UI/Menu/StudentMenu.cs
+++ src/UI/Menu/StudentMenu.cs
@@ -4,39 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace UI.Menu;
+
 public class StudentMenu : IView
 {
     public const string Request = "Create Request", ChangePassword = "Change Password", Exit = "Exit";
-    public static void Student_Menu()
+
+    public string GetOption()
     {
         var userSelection = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
+                new SelectionPrompt<string>()
                 .Title("Select an option")
                 .PageSize(2)
                 .AddChoices(new[] {
                     Request, ChangePassword, Exit
-        }));
-        switch (userSelection)
-        {
-            case Request:
-                RequestMenu.Request_General_Menu();
-                break;
-            case ChangePassword:
-                // ChangePassword();
-                break;
-            case Exit:
-                Environment.Exit(0);
-                break;
-        }
+                    }));
+        return userSelection;
     }
 
     public void DoOption(string option)
     {
-        throw new NotImplementedException();
+        switch (option) {
+        case Request:
+            ViewManager.Next(new RequestMenu());
+            break;
+        case ChangePassword:
+            // TODO: Implement ChangePassword
+            // ChangePassword();
+            break;
+        case Exit:
+            Environment.Exit(0);
+            break;
+        }
     }
 
-    public string GetOption()
-    {
-        throw new NotImplementedException();
-    }
 }
